9cfbbcda8331907710a4a384094ea0485aa67d4a
Zircoz <dixit.setu@gmail.com>
2020-10-05 18:57:21 +0530

    tried https: data undefined bug -get form, username extraction completed

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var app = express();
 const port = 3000 || process.env.PORT;
+var https = require('https');
+
+
 
 
 app.set('view engine', 'ejs');
@@ -13,10 +16,45 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-  res.render('index.ejs')
+app.get('/statPub', function(req, res){
+  res.render('statPub.ejs')
 })
 
+app.get('/', function (req, res) {
+  res.redirect('/statPub')
+})
+
+app.post('/statPub', function(req, res, next){
+  var url  = req.body.channelUrl
+  var username = ''
+  if (url.slice(-1)=='/'){
+    url = url.slice(0, url.length-1)
+  }
+  if (url.indexOf('/c/')!=-1) {
+    username = url.slice((url.indexOf('/c/')+3))
+  }
+  else if (url.indexOf('/channel/')!=-1) {
+    username = url.slice((url.indexOf('/channel/')+9))
+  } else {
+    res.send('invalid url')
+  }
+  let a='';
+  https.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername=thevenusproject&key=AIzaSyAZHmeCtdm0T_IOGeuG3SdUJ5KuOh7X2xQ', (res) => {
+    //console.log('statusCode:', res.statusCode);
+    //console.log('headers:', res.headers);
+    res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+
+
+  }).on('error', (e) => {
+    console.error('error',e);
+  });
+
+  //console.log(url)
+  //console.log(username)
+  res.send('')
+})
 
 
 
