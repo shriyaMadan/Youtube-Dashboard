6b789b11551aa93b790a2fcf12576f60c13180a6
Zircoz <dixit.setu@gmail.com>
2020-10-08 20:14:38 +0530

    making them equal (#1)
    * Create README.md
    
    * added view for results
    
    * result displays
    
    * add styling and render on same page
    
    Co-authored-by: Shriya Madan <38863530+shriyaMadan@users.noreply.github.com>
    Co-authored-by: Shriya Madan <shriyamadan333@gmail.com>

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var app = express();
 const port = 3000 || process.env.PORT;
-var https = require('https');
 const axios = require('axios').default;
-
-
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -17,14 +14,18 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/statPub', function(req, res){
-  res.render('statPub.ejs')
+  res.render('statPub', { subsCount: "", videoCount: "", viewCount: "" })
 })
 
 app.get('/', function (req, res) {
   res.redirect('/statPub')
 })
 
-const key = 'AIzaSyAZHmeCtdm0T_IOGeuG3SdUJ5KuOh7X2xQ';
+app.get('/result',function(req,res){
+  res.render('result.ejs')
+})
+
+const key = '';  //personal 
 
 app.post('/statPub', function(req, res, next){
   var url  = req.body.channelUrl
@@ -56,17 +57,20 @@ app.post('/statPub', function(req, res, next){
   }
   axios.get(url)
     .then(function (response) {
-      // handle success
-      console.log(response.data.items[0].statistics);
-      res.send(response.data.items[0].statistics);
+      //res.send(response.data.items[0].statistics);
+      var subsCount = response.data.items[0].statistics.subscriberCount;
+      var viewCount = response.data.items[0].statistics.viewCount;
+      //var subsHidden = response.data.items[0].statistics.hiddenSubscriberCount;
+      var videoCount = response.data.items[0].statistics.videoCount;
+
+      res.render('statPub', { subsCount: subsCount,videoCount:videoCount, viewCount: viewCount })
+      
     })
     .catch(function (error) {
-      // handle error
-      console.log(error);
+      console.log(error)
       res.send(error)
     })
     .then(function () {
-      // always executed
     });
   //console.log(url)
   //console.log(username)
