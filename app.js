fbd32f8d1b07f438f2deadf6d1b40423c42db88d
Aarush-Goyal <goyalaarush92@gmail.com>
2020-12-20 12:01:09 +0530

    c-3 added channel thumbnails

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var http = require('http');
-var helmet = require('helmet');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var http = require("http");
+// var helmet = require('helmet');
 var app = express();
-var port = process.env.PORT|| 3000;
+var port = process.env.PORT || 3000;
 
-var indexRouter = require('./routes/index.js');
-require('dotenv').config();
-const axios = require('axios').default;
+var indexRouter = require("./routes/index.js");
+require("dotenv").config();
+const axios = require("axios").default;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(helmet());
-app.use('/', indexRouter);
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(helmet());
+app.use("/", indexRouter);
 
-app.listen(port,()=>{
-  console.log('listening on',port);
+app.listen(port, () => {
+  console.log("listening on", port);
 });
